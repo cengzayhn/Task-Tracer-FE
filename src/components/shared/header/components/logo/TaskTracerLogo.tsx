2f536ae5e0c210b814e9c694c32cdc375d278d3b
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-03-26 10:35:44 +0300

    logo has been fixed in the header

--- src/components/shared/header/components/logo/TaskTracerLogo.tsx
+++ src/components/shared/header/components/logo/TaskTracerLogo.tsx
@@ -5,10 +5,7 @@ export const TaskTracerLogo = ()=> {
     return(
         <>
             <div className='container'>
-                <div className='logo_container'>
-                    <img src="/resources/TaskTracerLogo.jpeg" alt="Logo" className="logo" /> 
-                    <span className='text'>TaskTracer</span>
-                </div>
+                    <img src="/resources/TaskTracerExtendedLogo.png" alt="Logo" className="logo" /> 
             </div>
         </>
     )
