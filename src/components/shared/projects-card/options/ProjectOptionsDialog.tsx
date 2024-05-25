d296134d8200e62329bd1997940879ec86401425
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-05-25 20:32:03 +0300

    handled styling of edit , delete project dialog

--- src/components/shared/projects-card/options/ProjectOptionsDialog.tsx
+++ src/components/shared/projects-card/options/ProjectOptionsDialog.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import { Dialog, DialogActions, DialogContent, DialogTitle, Button, TextField } from '@mui/material';
+import { IProject } from 'modal/Project';
 
 interface ProjectOptionsDialogProps {
     open: boolean;
     isEdit: boolean;
+    selectedProject?: IProject;
     handleClose: () => void;
 }
 
 const ProjectOptionsDialog: React.FC<ProjectOptionsDialogProps> = (props) => {
-    const { open, isEdit, handleClose } = props;
+    const { open,selectedProject, isEdit, handleClose } = props;
 
     return (
         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>{isEdit ? 'Edit Project' : 'Delete Project'}</DialogTitle>
-            <DialogContent>
+            <DialogTitle>{isEdit ? `Edit ${selectedProject?.name}` : `Delete ${selectedProject?.name}`}</DialogTitle>
+            <DialogContent sx={{ marginTop: 5 }}>
                 {isEdit ? (
                     <>
-                        <TextField label="New Project Name" fullWidth />
-                        <TextField label="New Project Description" fullWidth multiline />
-                        <TextField label="New Project Deadline" fullWidth type="date" />
+                        <TextField 
+                            label="Project Name" 
+                            fullWidth 
+                            defaultValue={selectedProject?.name} 
+                            sx={{ marginBottom: 2 }}
+                        />
+                        <TextField
+                            label="Project Status"
+                            fullWidth 
+                            defaultValue={selectedProject?.isOpen ? "Open" : "Closed"} 
+                            sx={{ marginBottom: 2 }}
+                            />
+                        <TextField 
+                            label="Members" 
+                            fullWidth 
+                            defaultValue={selectedProject?.memberIdList.join('; ')} 
+                            sx={{ marginBottom: 2 }}
+                        />
                     </>
                 ) : (
                     <p>Are you sure you want to delete this project?</p>
@@ -28,7 +45,7 @@ const ProjectOptionsDialog: React.FC<ProjectOptionsDialogProps> = (props) => {
                 <Button onClick={handleClose} color="primary">
                     Cancel
                 </Button>
-                <Button onClick={handleClose} color="primary" autoFocus>
+                <Button onClick={handleClose} color={isEdit ? 'success' : 'error'} autoFocus>
                     {isEdit ? 'Save' : 'Delete'}
                 </Button>
             </DialogActions>
--- src/container/projects/TaskTracerProjectsContainer.tsx
+++ src/container/projects/TaskTracerProjectsContainer.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './styles.css';
-import { Button, Grid, Paper, Typography } from '@mui/material';
+import { Button, Grid, Paper } from '@mui/material';
 import TaskTracerProjectsCard from '../../components/shared/projects-card/TaskTracerProjectsCard';
 import { useNavigate } from 'react-router-dom';
 import { IProject } from 'modal/Project';
 import TaskTracerDialog from '../../components/shared/task/dialog/TaskTracerDialog';
 import ProjectOptionsDialog from '../../components/shared/projects-card/options/ProjectOptionsDialog';
+
 interface TaskTracerProjectsContainerProps {
     projectId: string;
     setProjectId: Function;
@@ -29,106 +30,111 @@ const TaskTracerProjectsContainer: React.FC<TaskTracerProjectsContainerProps> = 
     const [openOptionsDialog, setOpenOptionsDialog] = useState(false);
     const [isEditMode, setIsEditMode] = useState(false);
     const [projectName, setProjectName] = useState<string>("");
+    const [selectedProject, setSelectedProject] = useState<IProject | undefined>(undefined);
     const [contextMenuPosition, setContextMenuPosition] = useState<{ x: number; y: number } | null>(null);
 
     const handleOpenDialog = () => {
-    setOpenDialog(true);
+        setOpenDialog(true);
     };
 
-    const handleProjectClick = (id: string) => {
-    setProjectId(id);
-    navigate(`/projects/${id}`);
+    const handleProjectClick = (data: IProject) => {
+        setProjectId(data.id);
+        setSelectedProject(data);
+        navigate(`/projects/${data.id}`);
     };
 
-    const handleProjectRightClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, id: string) => {
-    event.preventDefault();
-    setProjectId(id);
-    setContextMenuPosition({ x: event.clientX, y: event.clientY });
-    console.log("id : ", id );
+    const handleProjectRightClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, project: IProject) => {
+        event.preventDefault();
+        setProjectId(project.id);
+        setSelectedProject(project);
+        setContextMenuPosition({ x: event.clientX, y: event.clientY });
+        console.log("id : ", project.id );
     };
 
     const handleCloseContextMenu = () => {
-    setContextMenuPosition(null);
+        setContextMenuPosition(null);
     };
 
     React.useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-        if (contextMenuPosition && !event.target) return;
-        const paper = document.getElementById('context-menu-paper');
-        if (paper && !paper.contains(event.target as Node)) {
-        setContextMenuPosition(null);
-        }
-    };
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-        document.removeEventListener('click', handleClickOutside);
-    };
+        const handleClickOutside = (event: MouseEvent) => {
+            if (contextMenuPosition && !event.target) return;
+            const paper = document.getElementById('context-menu-paper');
+            if (paper && !paper.contains(event.target as Node)) {
+                setContextMenuPosition(null);
+            }
+        };
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
     }, [contextMenuPosition]);
-
 
     const handleEditProject = () => {
         setIsEditMode(true);
         setOpenOptionsDialog(true);
+        handleCloseContextMenu();
     }
 
     const handleDeleteProject = () => {
         setIsEditMode(false);
         setOpenOptionsDialog(true);
+        handleCloseContextMenu();
     }
 
     return (
-    <React.Fragment>
-        <Grid container className="button-container">
-        <Grid item xs={2} />
-        <Grid item xs={8} className='taskcreate-container'>
-            {projectId.length > 1 && <Button className='taskcreate-button' variant='contained' color='success' onClick={handleOpenDialog}>+</Button>}
-        </Grid>
-        <Grid item xs={2} />
-        </Grid>
-        <Grid container spacing={5} style={{ marginTop: '0.5vh' }}>
-        {dummyData.map((data, index) => (
-            <Grid item xs={3} key={index}>
-            <div className='projects-link' onClick={() => handleProjectClick(data.id)} onContextMenu={(e) => handleProjectRightClick(e, data.id)}>
-                <TaskTracerProjectsCard name={data.name} />
-            </div>
+        <React.Fragment>
+            <Grid container className="button-container">
+                <Grid item xs={2} />
+                <Grid item xs={8} className='taskcreate-container'>
+                    {projectId.length > 1 && <Button className='taskcreate-button' variant='contained' color='success' onClick={handleOpenDialog}>+</Button>}
+                </Grid>
+                <Grid item xs={2} />
             </Grid>
-        ))}
-        </Grid>
-        <TaskTracerDialog 
-        openDialog={openDialog} 
-        setOpenDialog={setOpenDialog} 
-        mode={"create project"} 
-        taskTitle={projectName} 
-        setTaskTitle={setProjectName} 
-        username="currentUsername" 
-        date="currentDate" 
-        />
-        {contextMenuPosition && (
-            <Paper
-            id="context-menu-paper"
-            style={{
-            position: 'fixed',
-            top: contextMenuPosition.y,
-            left: contextMenuPosition.x,
-            zIndex: 9999,
-            padding: '8px',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '8px',
-            }}
-            onClick={handleCloseContextMenu}
-        >
-            <Button variant='text' color='primary' onClick={handleEditProject}>Edit Project</Button>
-            <Button variant='text' color='error' onClick={handleDeleteProject}>Delete Project</Button>
-        </Paper>
-        )}
-        <ProjectOptionsDialog 
+            <Grid container spacing={5} style={{ marginTop: '0.5vh' }}>
+                {dummyData.map((data, index) => (
+                    <Grid item xs={3} key={index}>
+                        <div className='projects-link' onClick={() => handleProjectClick(data)} onContextMenu={(e) => handleProjectRightClick(e, data)}>
+                            <TaskTracerProjectsCard name={data.name} />
+                        </div>
+                    </Grid>
+                ))}
+            </Grid>
+            <TaskTracerDialog 
+                openDialog={openDialog} 
+                setOpenDialog={setOpenDialog} 
+                mode={"create project"} 
+                taskTitle={projectName} 
+                setTaskTitle={setProjectName} 
+                username="currentUsername" 
+                date="currentDate" 
+            />
+            {contextMenuPosition && (
+                <Paper
+                    id="context-menu-paper"
+                    style={{
+                        position: 'fixed',
+                        top: contextMenuPosition.y,
+                        left: contextMenuPosition.x,
+                        zIndex: 9999,
+                        padding: '8px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '8px',
+                    }}
+                    onClick={handleCloseContextMenu}
+                >
+                    <Button variant='text' color='primary' onClick={handleEditProject}>Edit Project</Button>
+                    <Button variant='text' color='error' onClick={handleDeleteProject}>Delete Project</Button>
+                </Paper>
+            )}
+            <ProjectOptionsDialog 
                 open={openOptionsDialog} 
                 isEdit={isEditMode} 
+                selectedProject={selectedProject}
                 handleClose={() => setOpenOptionsDialog(false)} 
             />
-    </React.Fragment>
+        </React.Fragment>
     );
-    }
+}
 
-    export default TaskTracerProjectsContainer;
+export default TaskTracerProjectsContainer;
