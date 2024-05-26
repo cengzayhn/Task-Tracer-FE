a324e98f600c39e87945755d64eb55c839744b3e
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-05-26 17:27:50 +0300

    create user implementation

--- src/components/shared/singup/TaskTracerSignUpComponent.tsx
+++ src/components/shared/singup/TaskTracerSignUpComponent.tsx
@@ -1,44 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 import './styles.css';
+import { createUser } from '../../../service/userService';
+import { IUser } from '../../../modal/User';
 
 interface TaskTracerSignUpComponentProps {
   setShowLogin: Function;
 }
 
-const TaskTracerSignUpComponent:React.FC<TaskTracerSignUpComponentProps> = ({setShowLogin}) => {
+const TaskTracerSignUpComponent: React.FC<TaskTracerSignUpComponentProps> = ({ setShowLogin }) => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    try{
+      const user:IUser = await createUser(name, surname, username, password);
+      console.log("User created :", user);
+      setName('');
+      setSurname('');
+      setUsername('');
+      setPassword('');
+      window.location.href = '/login';
+    }catch(error){
+      console.error("Error occured while creating user", error)
+    }
+
+  };
+
   return (
     <div className="signup-container">
       <Grid container justifyContent="center" alignItems="center">
         <Grid item xs={12} md={6} lg={4}>
           <div className="form-container">
-          <div className="logo-container">
-            <img src="/resources/TaskTracerLogo.jpeg" className="tasktracer-logo" alt="Task Tracer Logo" />
-            <span className="tasktracer-name">TaskTracer</span>
-          </div>
+            <div className="logo-container">
+              <img src="/resources/TaskTracerLogo.jpeg" className="tasktracer-logo" alt="Task Tracer Logo" />
+              <span className="tasktracer-name">TaskTracer</span>
+            </div>
             <Typography variant="h5" className="form-title">
               Sign Up for TaskTracer
             </Typography>
             <div className="signup-input-container">
               <Typography className="input-label">Name</Typography>
-              <TextField className="input-textfield" variant="outlined" />
+              <TextField
+                className="input-textfield"
+                variant="outlined"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="signup-input-container">
               <Typography className="input-label">Surname</Typography>
-              <TextField className="input-textfield" variant="outlined" />
+              <TextField
+                className="input-textfield"
+                variant="outlined"
+                value={surname}
+                onChange={(e) => setSurname(e.target.value)}
+              />
             </div>
             <div className="signup-input-container">
-              <Typography className="input-label">Email</Typography>
-              <TextField className="input-textfield" variant="outlined" />
+              <Typography className="input-label">Username</Typography>
+              <TextField
+                className="input-textfield"
+                variant="outlined"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="signup-input-container">
               <Typography className="input-label">Password</Typography>
-              <TextField className="input-textfield" type="password" variant="outlined" />
+              <TextField
+                className="input-textfield"
+                type="password"
+                variant="outlined"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <Typography className="input-text">
-              Already have an account? <span className="input-login" onClick={()=> setShowLogin(true)}>Login</span>
+              Already have an account? <span className="input-login" onClick={() => setShowLogin(true)}>Login</span>
             </Typography>
-            <Button variant="contained" color="success" size="large" className="signup-button">
+            <Button
+              variant="contained"
+              color="success"
+              size="large"
+              className="signup-button"
+              onClick={handleSignUp}
+            >
               Sign Up
             </Button>
           </div>
---
+++ src/service/userService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseURL = 'http://localhost:8081/task-tracer/';
+const userURL = 'user/';
+
+export const createUser = async (name: string, surname: string, username: string, password: string ) => {
+    const requestBody = {
+        name: name,
+        surname: surname,
+        username: username,
+        password: password
+    }
+    try{
+        const response = await axios.post(baseURL+userURL+'create',requestBody);
+        return response.data; 
+    }catch(error){
+        console.error("User creation error :", error);
+        throw error;
+    }
+}
