2d0b1cbff5d2762fbd5f8ecd03516ae1e661dbe3
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-05-26 23:51:10 +0300

    commit before closing

--- src/components/shared/kanban/TaskTracerDashboardComponent.tsx
+++ src/components/shared/kanban/TaskTracerDashboardComponent.tsx
@@ -10,13 +10,6 @@ interface TaskTracerDashboardComponentProps {
   projectId: string;
 }
 
-const realData: ITask[] = [
-  { projectId: 'project_id_1', id: '1', title: 'Ödevler yapılacak', description: 'Bu bir açıklamadır 1.', createdBy: 'user_1', createdDate: '2024-05-25', state: IState.TODO },
-  { projectId: 'project_id_2', id: '2', title: 'Projeler yapılacak', description: 'Bu bir açıklamadır 2.', createdBy: 'user_2', createdDate: '2024-05-26', state: IState.IN_PROGRESS },
-  { projectId: 'project_id_3', id: '3', title: 'Lorem ipsum dolor sit amet, consectetur', description: 'Bu bir açıklamadır 3.', createdBy: 'user_3', createdDate: '2024-05-27', state: IState.IN_TEST },
-  { projectId: 'project_id_4', id: '4', title: 'Minus temporibus facere', description: 'Bu bir açıklamadır 4.', createdBy: 'user_4', createdDate: '2024-05-28', state: IState.DONE }
-];
-
 const TaskTracerDashboardComponent: React.FC<TaskTracerDashboardComponentProps> = (props) => {
   const { projectId } = props;
   const [tasks, setTasks] = React.useState<ITask[]>([]);
@@ -24,7 +17,7 @@ const TaskTracerDashboardComponent: React.FC<TaskTracerDashboardComponentProps> 
   React.useEffect(() => {
     const fetchData = async () => {
       try {
-        const tasks = await getTasksByProjectId("33e04a47-97cc-4641-9f9c-6f318dd32297");
+        const tasks = await getTasksByProjectId("9937f74c-7af2-4891-bd0c-f752da647cee");
         setTasks(tasks);
       } catch (error) {
         console.error("Error fetching tasks:", error);
